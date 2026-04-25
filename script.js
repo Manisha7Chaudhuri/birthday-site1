acddb3f3ecd243cc1ef299c84b8a5a76c3947e1f
Manisha Chaudhuri <manishachaudhuri9@gmail.com>
2026-04-25 15:07:11 +0530

    3rd

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ function startExperience() {
 
   nextSlide();
 }
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function nextSlide() {
+  slides[index].classList.remove("active");
+  index++;
+
+  if (index >= slides.length) index = slides.length - 1;
+
+  slides[index].classList.add("active");
+
+  controlMusic();
+}
+function controlMusic() {
+  const main = document.getElementById("main-music");
+  const special = document.getElementById("special-music");
+
+  // Example: play special music on slide 3 (index starts from 0)
+  if (index === 2) {
+    main.pause();
+    special.currentTime = 0;
+    special.play();
+  } else {
+    special.pause();
+    main.play();
+  }
+}
